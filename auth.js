99f94f3505ef5561b5496180961f82d151025ca7
kikomayorga <kikomayorga@gmail.com>
2014-03-08 18:20:59 -0500

    ahora si

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = '748783775208.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
